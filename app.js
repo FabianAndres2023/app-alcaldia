0974373a52eaed205d60efb5d05f09f2e4e818a8
FabianAndres2023 <fabian.mondragon01@uceva.edu.co>
2025-09-30 17:28:15 -0500

    Organizar enlaces

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ function goTo(screenId) {
   document.querySelectorAll('.screen').forEach(s => s.classList.remove('active'));
   document.getElementById(screenId).classList.add('active');
 }
+
+function goTo(screenId) {
+  document.querySelectorAll('.screen').forEach(s => s.classList.remove('active'));
+  document.getElementById(screenId).classList.add('active');
+}
