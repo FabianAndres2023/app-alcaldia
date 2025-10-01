e6527f4ff0ff8607430430d06773df4aca720f71
FabianAndres2023 <fabian.mondragon01@uceva.edu.co>
2025-10-01 08:31:23 -0500

    Cambios en diseño y funcionalidad

--- app.js
+++ app.js
@@ -2,8 +2,3 @@ function goTo(screenId) {
   document.querySelectorAll('.screen').forEach(s => s.classList.remove('active'));
   document.getElementById(screenId).classList.add('active');
 }
-
-function goTo(screenId) {
-  document.querySelectorAll('.screen').forEach(s => s.classList.remove('active'));
-  document.getElementById(screenId).classList.add('active');
-}
